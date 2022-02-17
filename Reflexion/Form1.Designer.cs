d800c7f04787b0a502b6dd5d977e7f71b60c58f2
Fuchs12 <jasonh@gso.schule.koeln>
2022-02-17 11:10:05 +0100

    Projektdateien hinzufügen.

---
+++ Reflexion/Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿
+namespace Reflexion
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timerGame = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timerGame
+            // 
+            this.timerGame.Tick += new System.EventHandler(this.timerGame_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.FrmReflexion_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FrmReflexion_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timerGame;
+    }
+}
+
---
+++ Reflexion/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Reflexion
+{
+    public partial class Form1 : Form
+    {
+        Rectangle spieler;
+        int anteilRechteckDimension = 10;
+        List<Rectangle> alleZiele = new List<Rectangle>();
+        Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            ResizeRedraw = true;
+
+            InitialisiereRechteck();
+
+            timerGame.Interval = 100;
+            timerGame.Start();
+        }
+
+        private void InitialisiereRechteck()
+        {
+            spieler = new Rectangle();
+            spieler.Width = ClientSize.Width / anteilRechteckDimension;
+            spieler.Height = ClientSize.Height / anteilRechteckDimension;
+            spieler.X = ClientSize.Width / 2 - spieler.Width / 2;
+            spieler.Y = ClientSize.Height / 2 - spieler.Height / 2;
+        }
+
+        private void FrmReflexion_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+
+            Brush brush = new SolidBrush(Color.Orange);
+            Pen pen = new Pen(Color.Black);
+
+            graphics.FillRectangle(brush, spieler);
+            graphics.DrawRectangle(pen, spieler);
+
+            foreach (Rectangle rectangle in alleZiele)
+            {
+                graphics.FillRectangle(new SolidBrush(Color.Red), rectangle);
+            }
+
+        }
+
+        private void timerGame_Tick(object sender, System.EventArgs e)
+        {
+            spawnTargets();
+        }
+
+        int spawnRate = 14;
+        int spawnZaehler = 0;
+        private void spawnTargets()
+        {
+            if (spawnZaehler >= spawnRate)
+            {
+                Rectangle rectangle = new Rectangle();
+                do
+                {
+                    rectangle.X = random.Next(0, ClientSize.Width);
+                    rectangle.Y = random.Next(0, ClientSize.Height);
+                } while (rectangle.IntersectsWith(spieler));
+
+                rectangle.Width = ClientSize.Width / 15;
+                rectangle.Height = rectangle.Width;
+
+                alleZiele.Add(rectangle);
+                spawnZaehler = 0;
+            }
+            else
+            {
+                spawnZaehler++;
+            }
+        }
+
+        int schrittweite = 10;
+        private void FrmReflexion_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                spieler.Y -= schrittweite;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                spieler.Y += schrittweite;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                spieler.X -= schrittweite;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                spieler.X += schrittweite;
+            }
+
+            Refresh();
+        }
+    }
+}
